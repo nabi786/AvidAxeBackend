a3f4d53ba1dea12888132f55c85eeaa1981bf48e
Nabi Bux <rehanpardesi2018@gmail.com>
2023-07-27 18:08:41 +0500

    updated

--- index.js
+++ index.js
@@ -2,9 +2,15 @@ require("dotenv").config()
 const express = require("express")
 const app = express()
 const PORT= 3000
+const cors = require("cors")
+
+
 
 app.use(express.json())
 app.use(express.urlencoded({extended: false}))
+app.use(cors());
+
+
 
 // importing routes 
 app.use('/api/', require("./routes/compaign"))

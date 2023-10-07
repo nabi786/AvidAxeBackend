5d18e248eac4d79770de77a8f74c40f143ce3031
Nabi Bux <rehanpardesi2018@gmail.com>
2023-10-07 14:52:23 +0500

    update code

--- index.js
+++ index.js
@@ -6,8 +6,8 @@ const PORT = process.env.PORT;
 const cors = require("cors");
 
 app.use(cors());
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.json({ limit: '10mb' }));
+app.use(express.urlencoded({ extended: false ,limit: '10mb'}));
 
 // importing routes
 app.use("/api/", require("./routes/compaign"));
